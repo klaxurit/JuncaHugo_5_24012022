22dbe672be4f82f0f3975ffe460a46319e6ce79f
Axurit <hjunca@skalv-studio.fr>
2022-02-02 17:04:20 +0100

    add src folder

---
+++ BlogPHP/src/Controllers/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+
+class BlogController {
+
+    public function welcome()
+    {
+        //
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function show(int $id)
+    {
+        //
+    }
+}
