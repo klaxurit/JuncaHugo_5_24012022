8e4c0a38a6885b998b48686a781343cbcf2587ce
Axurit <hjunca@skalv-studio.fr>
2022-05-26 12:34:33 +0200

    work on registration

--- src/Controllers/SecurityController.php
+++ src/Controllers/SecurityController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Model\User;
 use App\Core\Controller;
 
 class SecurityController extends Controller
 {
-  
+
   /**
    * Return the login page
    *
@@ -14,9 +15,10 @@ class SecurityController extends Controller
    */
   public function login()
   {
-      $this->twig->display('client/pages/login.html.twig');
+
+    $this->twig->display('client/pages/login.html.twig');
   }
-  
+
   /**
    * return the register page
    *
@@ -24,6 +26,42 @@ class SecurityController extends Controller
    */
   public function register()
   {
-      $this->twig->display('client/pages/register.html.twig');
+    // check if form has been sent
+    if (!empty($_POST)) {
+      // form has been sent
+      // check if all reaquired field are completed
+      if (
+        isset($_POST["nom"], $_POST["prenom"], $_POST["surnom"], $_POST["email"], $_POST["password"])
+        && !empty($_POST["nom"])
+        && !empty($_POST["prenom"])
+        && !empty($_POST["surnom"])
+        && !empty($_POST["email"])
+        && !empty($_POST["password"])
+      ) {
+        // form is complete
+        // get & protect datas
+        $nom = strip_tags($_POST["nom"]);
+
+        // email must be an email
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+          die("L'addresse email est incorrect.");
+        }
+
+        // hash password using password ARGON2ID 
+        // (ARGON2ID algorithm most powerfull than BCRYPT and ARGON2I)
+        $password = password_hash($_POST["password"], PASSWORD_ARGON2ID);
+
+        // check if entered password are the same
+        if ($_POST["password_confirmation"] != $_POST["password"]) {
+          die("Les mots de passe entrés sont différents");
+        }
+
+        $sql = "INSERT INTO `user`(`nom`, `prenom`, `surnom`, `email`, `password`) VALUES (:nom, :prenom, :surnom, :email, '$password')";
+        $req = $this->pdo->prepare();
+      } else {
+        die("Le formulaire est incomplet");
+      }
+    }
+    $this->twig->display('client/pages/register.html.twig');
   }
 }
--- src/Managers/UserManager.php
+++ src/Managers/UserManager.php
@@ -3,15 +3,17 @@
 namespace App\Managers;
 
 use PDO;
+use App\Model\User;
 use App\Core\Manager;
 
 class UserManager extends Manager
 {
-  public function __construct(){
+  public function __construct()
+  {
     parent::__construct();
   }
 
-    
+
   /**
    * find all users in db
    *
@@ -24,5 +26,14 @@ class UserManager extends Manager
     $req->execute();
     $datas = $req->fetchAll(PDO::FETCH_COLUMN, 2);
     return $datas;
+  }
+
+  public function createUser()
+  {
+    $sql = "INSERT INTO `user`(`nom`, `prenom`, `surnom`, `email`, `password`) VALUES (:nom, :prenom, :surnom, :email, '$password')";
+    $req = $this->pdo->prepare($sql);
+    $req->execute();
+    $datas = $req->fetch();
+    return new User($datas);
   }
 }
